b89808129ed53288beac5057e98e5bea82a315b2
Sachin kumar <singhsachin54896@gmail.com>
2023-09-27 00:51:15 +0530

    Add files via upload

---
+++ first.java
@@ -0,0 +1,12 @@
+import java.util.*;
+public class first {
+    public static void main(String[] args){
+        Scanner sc = new Scanner(System.in);
+        System.out.println("enter first number");
+        int a = sc.nextInt();
+
+    }
+
+
+
+}
